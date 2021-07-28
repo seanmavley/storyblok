fabd5dae12aedeb20a7b47ca6cadb6a68b39e80a
KhoPhi <seanmavley@gmail.com>
2021-07-28 15:14:56 +0000

    removed unused code

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -21,8 +21,6 @@ async function sleep(ms: any) {
 }
 
 indexRouter.post('/initiate', (req, res) => {
-  console.log('Initiation');
-
   let space: any;
   let stories: any[];
   let formData: iFormData = req.body
@@ -36,12 +34,9 @@ indexRouter.post('/initiate', (req, res) => {
   .then((response: any) => {
     stories = response.data.stories;
     
-    console.log(stories);
-
     stories.forEach(async (element: any, index: any) => {
       const content: string = element.content.component;
       const replaced_content = content.replace(formData.search, formData.replace);
-
       await sleep(1000 * index)
         .then(() => {
           const payload = {
@@ -49,8 +44,8 @@ indexRouter.post('/initiate', (req, res) => {
               "name": element.name,
               "slug": element.name.replace(/ +/g, '-').toLowerCase(),
               "content": {
-                "component": replaced_content,
-                "body": []
+                "component": "page",
+                "body": [{ content: replaced_content}]
               }
             },
             "force_update": 1,
@@ -65,71 +60,19 @@ indexRouter.post('/initiate', (req, res) => {
                 Authorization: process.env.MAPI_TOKEN
               }
             }
-          ).then((res: any) => {
-            console.log(res.data);
+          ).then((putResponse: any) => {
+            res.json({
+              'status': true
+            })
           })
           .catch(err => {
-            console.log(err.data);
+            console.log(err);
           })
-
-          console.log('Last mile payload')
         })
     });
 
   })
 
-  res.json({
-    'status': true
-  })
-})
-
-
-indexRouter.get('/space', (req, res) => {
-  axios.get(`https://api.storyblok.com/v1/cdn/spaces/me?token=${process.env.CDN_TOKEN}`)
-    .then((response: any) => {
-      res.json(response.data);
-    })
-})
-
-indexRouter.get('/story', (req, res) => {
-  axios.get(`https://api.storyblok.com/v1/cdn/stories?token=${process.env.CDN_TOKEN}`)
-    .then((response: any) => {
-      res.json(response.data);
-    })
-})
-
-indexRouter.post('/story', (req, res) => {
-  const data = req.body;
-
-  const payload = {
-    "story": {
-      "name": data.name,
-      "slug": data.name.replace(/ +/g, '-').toLowerCase(),
-      "content": {
-        "component": data.content,
-        "body": []
-      }
-    },
-    "force_update": 1,
-    "publish": 1
-  }
-
-  axios.put(
-    `https://mapi.storyblok.com/v1/spaces/${data.space_id}}/stories/${data.story_id}`,
-    payload,
-    {
-      headers: {
-        Authorization: process.env.MAPI_TOKEN
-      }
-    }
-  ).then((res: any) => {
-    res.json(res.data);
-  })
-    .catch((err) => {
-      res.json({
-        data: err
-      })
-    })
 })
 
 // Export the base-router
